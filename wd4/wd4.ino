ac6cb2cea8cd288b5e0362f09f43201d11720f5c
farinajf <farinajf@gmail.com>
2019-10-05 20:29:28 +0200

    wd4x4

--- wd4/wd4.ino
+++ wd4/wd4.ino
@@ -1,9 +1,36 @@
-void setup() {
-  // put your setup code here, to run once:
+/**
+ * 4WD CAR
+ */
+#include "wd4Globals.h"
+#include "pinChangeInt.h"
+#include "pins.h"
+#include "voltage.h"
+#include "ultrasonic.h"
 
+WD4::Voltage voltage;
+
+/**
+ * 
+ */
+void setup() {
+  Serial.begin(9600);
+
+  voltage.init();
+  ultrasonicInit();
 }
 
+/**
+ * Loop
+ */
 void loop() {
-  // put your main code here, to run repeatedly:
+  voltage.isVoltageLow();
 
+  getDistance();
+  getDistanceLeft();
+  getDistanceRight();
+
+  delay(200);
+
+  Serial.print("Distance: "); Serial.print(distance); Serial.print("   Distance right: "); Serial.print(distanceRight); Serial.print("   Distance left: "); Serial.print(distanceLeft);
+  Serial.println(); 
 }
