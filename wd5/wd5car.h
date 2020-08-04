840136ed74a92690a5134a2f748bd9bf5f576d3d
fran <farinajf@gmail.com>
2020-08-04 11:12:26 +0200

    WD5

--- wd5/wd5car.h
+++ wd5/wd5car.h
@@ -12,12 +12,12 @@ namespace WD5 {
       MotionModeEnum  _motionMode    = START;
       L298NEngine     _rightEngine;
       L298NEngine     _leftEngine;
-      bool            _alert    = false;
-      bool            _backward = false;
+      bool            _alert         = false;
       int             _forwardSpeed;    //velocidad es 100x(_velocidad/255)%
       int             _backwardSpeed;
       int             _turnSpeed;
-      int             _turnsInAlert  = 0;
+      int             _turnsCounter    = 0;
+      int             _backwardCounter = 0;
 
       bool _isStopped() const {return _motionMode == STOP;}
 
@@ -31,11 +31,12 @@ namespace WD5 {
           case START:      return "START";
           case STOP:       return "STOP";
           case ALERT:      return "ALERT";
-          case ALERT_FLIP: return "ALERT_FLIP";
           case FORWARD:    return "FORWARD";
           case BACKWARD:   return "BACKWARD";
           case TURN_LEFT:  return "TURN_LEFT";
           case TURN_RIGHT: return "TURN_RIGHT";
+          case FLIP_LEFT:  return "FLIP_LEFT";
+          case FLIP_RIGHT: return "FLIP_RIGHT";
         }
       }
 
@@ -44,12 +45,25 @@ namespace WD5 {
        * 
        ****************************************************************/
       void _setAlert(bool x) {
-        _alert        = x;
-        _turnsInAlert = 0;
+        _alert = x;
+      }
+
+      /****************************************************************
+       * MotionModeEnum _getFlipMode(Sensors sensors)
+       * 
+       * return:
+       * - FLIP_LEFT
+       * - FLIP_RIGHT
+       * 
+       ****************************************************************/
+      MotionModeEnum _getFlipMode(Sensors sensors) {
+        return _motionMode = (sensors.getSensorDistanceLeft() > sensors.getSensorDistanceRight()) ? FLIP_LEFT : FLIP_RIGHT;
       }
 
       /****************************************************************
        * MotionModeEnum _setMotionModeInAlert()
+       * 
+       * @deprecated
        * 
        ****************************************************************/
       MotionModeEnum _setMotionModeInAlert(Sensors sensors) {
@@ -67,30 +81,35 @@ namespace WD5 {
         if ((sensors.isLeftOK() == true) || (sensors.isRightOK() == true))
         {
           //3.1- Izquierda OK
-          if ((sensors.isLeftOK() == true) && (_turnsInAlert < 0)) return _motionMode = TURN_LEFT;
+          if ((sensors.isLeftOK() == true) && (_turnsCounter < 0)) return _motionMode = TURN_LEFT;
   
           //3.2.- Derecha OK
-          if ((sensors.isRightOK() == true) && (_turnsInAlert > 0)) return _motionMode = TURN_RIGHT;
+          if ((sensors.isRightOK() == true) && (_turnsCounter > 0)) return _motionMode = TURN_RIGHT;
 
           //3.3.- Giros alternados
-          if ((_turnsInAlert < 0) && (sensors.getSensorDistanceRight() > sensors.getSensorDistanceLeft()))  return _motionMode = BACKWARD;
-          if ((_turnsInAlert > 0) && (sensors.getSensorDistanceLeft()  > sensors.getSensorDistanceRight())) return _motionMode = BACKWARD;
+          if ((_turnsCounter < 0) && (sensors.getSensorDistanceRight() > sensors.getSensorDistanceLeft()))  return _motionMode = BACKWARD;
+          if ((_turnsCounter > 0) && (sensors.getSensorDistanceLeft()  > sensors.getSensorDistanceRight())) return _motionMode = BACKWARD;
 
           //3.4.- Mejor opcion
-          _motionMode = (sensors.getSensorDistanceLeft() > sensors.getSensorDistanceRight()) ? TURN_LEFT : TURN_RIGHT;
-          return _motionMode;
+          return _motionMode = (sensors.getSensorDistanceLeft() > sensors.getSensorDistanceRight()) ? TURN_LEFT : TURN_RIGHT;
         }
 
         //4.- Izquierda FAIL && Derecha FAIL && Forward FAIL
         if (sensors.isForwardOK() == false) return _motionMode = BACKWARD;
 
         //5-. Fin
-        return _motionMode = ALERT_FLIP;
+        return _motionMode = _getFlipMode(sensors);
       }
 
       /****************************************************************
-       * MotionModeEnum _setMotionModeInAlert2()
+       * MotionModeEnum _setMotionModeInAlert2(Sensors sensors)
        * 
+       * return:
+       *  - FORWARD
+       *  - BACKWARD
+       *  - TURN_RIGHT
+       *  - TURN_LEFT
+       *  
        ****************************************************************/
       MotionModeEnum _setMotionModeInAlert2(Sensors sensors) {
         //1.- Todo OK
@@ -108,19 +127,19 @@ namespace WD5 {
         if (sensors.isLateralOK() == true)
         {
           //3.1.- Seguir girando a derechas
-          if (_turnsInAlert > 0) return _motionMode = TURN_RIGHT;
+          if (_turnsCounter > 0) return _motionMode = TURN_RIGHT;
 
           //3.2.- Seguir girando a izquierdas
-          if (_turnsInAlert < 0) return _motionMode = TURN_LEFT;
+          if (_turnsCounter < 0) return _motionMode = TURN_LEFT;
 
           //3.3.- Seleccionar sentido de giro
-          if (_turnsInAlert == 0) return _motionMode = (sensors.getSensorDistanceLeft() > sensors.getSensorDistanceRight()) ? TURN_LEFT : TURN_RIGHT;
+          if (_turnsCounter == 0) return _motionMode = (sensors.getSensorDistanceLeft() > sensors.getSensorDistanceRight()) ? TURN_LEFT : TURN_RIGHT;
         }
 
         //4.- Lateral Derecha OK
         if (sensors.isLateralRightOK() == true)
         {
-          if ((sensors.isDistanceRightOK() == true) && (_turnsInAlert >= 0)) return _motionMode = TURN_RIGHT;
+          if ((sensors.isDistanceRightOK() == true) && (_turnsCounter >= 0)) return _motionMode = TURN_RIGHT;
 
           return _motionMode = BACKWARD;
         }
@@ -128,7 +147,7 @@ namespace WD5 {
         //5.- Lateral Izquierda OK
         if (sensors.isLateralLeftOK() == true)
         {
-          if ((sensors.isDistanceLeftOK() == true) && (_turnsInAlert <= 0)) return _motionMode = TURN_LEFT;
+          if ((sensors.isDistanceLeftOK() == true) && (_turnsCounter <= 0)) return _motionMode = TURN_LEFT;
 
           return _motionMode = BACKWARD;
         }
@@ -137,14 +156,22 @@ namespace WD5 {
         if (sensors.isDistanceRightOK() == false) return _motionMode = BACKWARD;
         if (sensors.isDistanceLeftOK()  == false) return _motionMode = BACKWARD;
         if (sensors.isForwardOK()       == false) return _motionMode = BACKWARD;
-        if (_backward                   == true)  return _motionMode = BACKWARD;
+        if (_backwardCounter            > 0)      return _motionMode = BACKWARD;
 
         return _motionMode = FORWARD;
       }
 
       /****************************************************************
-       * MotionModeEnum _setMotionMode()
+       * MotionModeEnum _setMotionMode(Sensors sensors)
        * 
+       * return:
+       *  - STOP
+       *  - FORWARD
+       *  - ALERT
+       *  - FLIP_LEFT
+       *  - FLIP_RIGHT
+       *  - _setMotionModeInAlert2()
+       *  
        ****************************************************************/
       MotionModeEnum _setMotionMode(Sensors sensors) {
         //1.- Comprobamos bateria
@@ -153,10 +180,13 @@ namespace WD5 {
         //2.- Estamos modo alerta
         if (_alert == true)
         {
+          //2.1.- Modo alerta
           _setMotionModeInAlert2(sensors);
-          
-          if ((_backward == true) && (_motionMode != BACKWARD)) return _motionMode = ALERT_FLIP;
 
+          //2.2.- Varios ciclos en modo BACKWARD => FLIP MODE
+          if ((_backwardCounter > 0) && (_motionMode != BACKWARD)) _motionMode = _getFlipMode(sensors);
+
+          //2.3.- OTHER CASE
           return _motionMode;
         }
 
@@ -200,8 +230,6 @@ namespace WD5 {
        * void backward()
        ****************************************************************/
       void backward() {
-        _backward = true;
-        
         _rightEngine.backward(_backwardSpeed);
         _leftEngine.backward (_backwardSpeed);
       }
@@ -223,13 +251,19 @@ namespace WD5 {
       }
 
       /****************************************************************
-       * void flip()
+       * void flipRight()
        ****************************************************************/
-      void flip() {
-        _backward = false;
-        
-        _rightEngine.backward(_turnSpeed);
-        _leftEngine.forward  (_turnSpeed);
+      void flipRight() {
+        turnRight();
+
+        delay(500);
+      }
+
+      /****************************************************************
+       * void flipLeft()
+       ****************************************************************/
+      void flipLeft() {
+        turnLeft();
 
         delay(500);
       }
@@ -251,15 +285,16 @@ namespace WD5 {
       void drive() {
         switch(_motionMode)
         {
-          case STOP:       stopCar();   _turnsInAlert = 0; break;
-          case ALERT:      stopCar();   _turnsInAlert = 0; break;
-          case FORWARD:    forward();   _turnsInAlert = 0; break;
-          case BACKWARD:   backward();  _turnsInAlert = 0; break;
-          case TURN_LEFT:  turnLeft();  _turnsInAlert--;   break;
-          case TURN_RIGHT: turnRight(); _turnsInAlert++;   break;
-          case ALERT_FLIP: flip();      _turnsInAlert = 0; break;
-          case START:      break;
-          default:         break;
+          case STOP:       _turnsCounter = 0; _backwardCounter = 0; stopCar();   break;
+          case ALERT:      _turnsCounter = 0; _backwardCounter = 0; stopCar();   break;
+          case FORWARD:    _turnsCounter = 0; _backwardCounter = 0; forward();   break;
+          case BACKWARD:   _turnsCounter = 0; _backwardCounter++;   backward();  break;
+          case TURN_LEFT:  _turnsCounter--;   _backwardCounter = 0; turnLeft();  break;
+          case TURN_RIGHT: _turnsCounter++;   _backwardCounter = 0; turnRight(); break;
+          case FLIP_LEFT:  _turnsCounter = 0; _backwardCounter = 0; flipLeft();  break;
+          case FLIP_RIGHT: _turnsCounter = 0; _backwardCounter = 0; flipRight(); break;
+          case START:      _turnsCounter = 0; _backwardCounter = 0; break;
+          default:         _turnsCounter = 0; _backwardCounter = 0; break;
         }
       }
 
@@ -277,7 +312,7 @@ namespace WD5 {
         //2.- Obtenemos el estado en funcion de la info de los sensores
         _setMotionMode(sensors);
 
-        if ((DEBUG == true) && ((millis() - _printPrevTime) > 500))
+        if ((DEBUG == true) && (millis() - _printPrevTime) > 500)
         {
           _printPrevTime = millis();
           
--- wd5/wd5globals.h
+++ wd5/wd5globals.h
@@ -1,17 +1,26 @@
 #ifndef WD5_GLOBALS_H
 #define WD5_GLOBALS_H
 
+/*
+#define MODO_DEBUG
+*/
+
 unsigned int DISTANCE_MAX               = 200;
 unsigned int DISTANCE_MIN               =  35;
 unsigned int DISTANCE_MIN_LATERAL       =  30;
 unsigned int DISTANCE_SUPER_MIN         =   5;
 float        VOLTAGE_UMBRAL             = 6.5;
+#ifdef MODO_DEBUG
+bool         DEBUG                      = true;
+int          FORWARD_SPEED              = 0;
+int          BACKWARD_SPEED             = 0;
+int          TURN_SPEED                 = 0;
+#else
+bool         DEBUG                      = false;
 int          FORWARD_SPEED              = 150;
 int          BACKWARD_SPEED             = 150;
 int          TURN_SPEED                 = 150;
-
-bool         DEBUG                      = true;
-
+#endif
 /**
  * 
  */
@@ -19,11 +28,12 @@ enum MotionModeEnum {
   START,
   STOP,
   ALERT,
-  ALERT_FLIP,
   FORWARD,
   BACKWARD,
   TURN_LEFT,
-  TURN_RIGHT
+  TURN_RIGHT,
+  FLIP_LEFT,
+  FLIP_RIGHT
 };
 
 enum AlertSensorEnum {
