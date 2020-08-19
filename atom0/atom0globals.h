95f0fe067dad7f2f53c59bc97ae93e185a466fa9
fran <farinajf@gmail.com>
2020-08-19 22:24:59 +0200

    rm rover

--- atom0/atom0globals.h
+++ atom0/atom0globals.h
@@ -7,9 +7,9 @@ unsigned int DISTANCE_MIN        =  30;
 unsigned int DISTANCE_SUPER_MIN  =   5;
 unsigned int PING_INTERVAL       =  50; // Intervalo entre PINGs
 int          FORWARD_SPEED_FAST  = 255;
-int          FORWARD_SPEEP_SLOW  = 100;
-int          BACKWARD_SPEED      = 100;
-int          TURN_SPEED          = 100;
+int          FORWARD_SPEEP_SLOW  = 150;
+int          BACKWARD_SPEED      = 150;
+int          TURN_SPEED          = 150;
 
 bool         DEBUG               = true;
 
