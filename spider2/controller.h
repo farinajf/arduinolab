00c071821c0cb9190f59d8d9ce5b51a5a0111e2c
fran <farinajf@gmail.com>
2021-08-29 14:40:37 +0200

    first commit

--- spider2/controller.h
+++ spider2/controller.h
@@ -15,9 +15,9 @@ namespace SPIDER {
       const float _BODY_LIFT_STEP_DISTANCE  =   1.0;
       const float _BODY_TWIST_STEP_DISTANCE =   1.25;
       const float _LEG_LIFT_STEP_DISTANCE   =   7.5;
-      const float _DISTANCIA_CICLO          =  50;
-      const float _ANGULO_GIRO              =  18;
-      const float _ALTURA_SUSPENSION        =  20;
+      const float _DISTANCIA_CICLO          =  45;
+      const float _ANGULO_GIRO              =  25;
+      const float _ALTURA_SUSPENSION        =  25;
       const float _MIN_ALPHA_INTERVAL       =   0;
 
 
